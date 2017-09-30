75365e90c1dbef8dcfb4f2afae6dd871c1dee64c
Fritz Gerald Santos <fritzgsantos@gmail.com>
2017-10-01 00:19:11 +0100

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,15 +4,8 @@
  * @author fritzgs
  *Student number 20071968
  *BSc in Computer Forensics - Year 2
-<<<<<<< HEAD
-=======
  * 
- * Known issues: 
- * 1) The clear button doesn't function fully - it will clear the display but the previous displayValue is still set.
- * 2) If the answer to an equation equals a negative value ... it will only display the ablsolute value.
- * 3) Some equations aren't resulting the right answer...
  *
->>>>>>> d0eb13363e97f3f0b19e45de1aef8333fd1e1d3d
  */
 public class Calculator
 {
@@ -29,9 +22,9 @@ public class Calculator
 	}
 	
 
-//	/**
-//	 * In case the window was closed, show it again.
-//	 */
+	/**
+	 * In case the window was closed, show it again.
+	 */
 	public void show()
 	{
 		gui.setVisible(true);
